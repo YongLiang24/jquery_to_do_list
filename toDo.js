87efcf4f99bac0c69777c91cff912f55e0a60b43
Yong Liang <yongliang24@gmail.com>
2019-06-17 17:51:48 -0700

    Update toDo.js
    added delete functionality to each items in the list.

--- toDo.js
+++ toDo.js
@@ -1,1 +1,12 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
 
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
